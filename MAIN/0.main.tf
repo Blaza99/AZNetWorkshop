528700f1906af798cada5efed076310551ff2f2c
Blaza99 <71960988+Blaza99@users.noreply.github.com>
2024-03-10 11:37:16 +0100

    Update 0.main.tf

--- MAIN/0.main.tf
+++ MAIN/0.main.tf
@@ -5,19 +5,11 @@ provider "azurerm" {
   features {}
 }
 
-TODO
 terraform {
  backend "azurerm" {
+   resource_group_name  = "tfStateRG"
    storage_account_name = "tfstatemeritobz" 
    container_name       = "tfstate1" 
-   key                  = "tfstate" # "cloudeng.shared.terraform.tfstate" #tfstatemeritobz/terraform.tfstate  
-   access_key           = "tfstatemeritobz.tf" 
+   key                  = "tf.tfstate"
  }
 }
-
-
-    # backendServiceArm: 'Azure for Students(3cedcdab-0de1-42ac-acd8-03b2b69fad02)'
-    # backendAzureRmResourceGroupName: 'tfStateRG'
-    # backendAzureRmStorageAccountName: 'tfstatemeritobz'
-    # backendAzureRmContainerName: 'tfstate1'
-    # backendAzureRmKey: 'tfstatemeritobz/terraform.tfstate'
