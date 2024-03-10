a9ba80c73210631ad56484899a78e99ae896e7c0
blaza99 <blazej.zachcial@gmail.com>
2024-03-10 11:25:34 +0100

    main tf changed

--- MAIN/0.main.tf
+++ MAIN/0.main.tf
@@ -5,12 +5,19 @@ provider "azurerm" {
   features {}
 }
 
-#TODO
-#terraform {
-#  backend "azurerm" {
-#    storage_account_name = "<ACCOUNT NAME>" 
-#    container_name       = "<CONTAINER>" 
-#    key                  = "cloudeng.shared.terraform.tfstate"  
-#    access_key  = 
-#  }
-#}
+TODO
+terraform {
+ backend "azurerm" {
+   storage_account_name = "tfstatemeritobz" 
+   container_name       = "tfstate1" 
+   key                  = "tfstate" # "cloudeng.shared.terraform.tfstate" #tfstatemeritobz/terraform.tfstate  
+   access_key           = "tfstatemeritobz.tf" 
+ }
+}
+
+
+    # backendServiceArm: 'Azure for Students(3cedcdab-0de1-42ac-acd8-03b2b69fad02)'
+    # backendAzureRmResourceGroupName: 'tfStateRG'
+    # backendAzureRmStorageAccountName: 'tfstatemeritobz'
+    # backendAzureRmContainerName: 'tfstate1'
+    # backendAzureRmKey: 'tfstatemeritobz/terraform.tfstate'
